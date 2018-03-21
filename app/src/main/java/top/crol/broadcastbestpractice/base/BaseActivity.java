5868a8c1c0e5d730733fd7fe8df0ff0f94dafdbc
hhjyhxw <512162086@qq.com>
2018-03-21 11:19:46 +0800

    broadcastbestpractice

---
+++ app/src/main/java/top/crol/broadcastbestpractice/base/BaseActivity.java
@@ -0,0 +1,24 @@
+package top.crol.broadcastbestpractice.base;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import top.crol.broadcastbestpractice.activityUtil.ActivityCollector;
+
+/**
+ * Created by z on 2018/3/21.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityCollector.addActivity(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
+    }
+}
